d4c836e8318a8894ed1d0d709866c5d07a1df0fe
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-02-14 04:18:13 +0200

    updated catch example

--- src/examples/rxjs/error-handling/catch.js
+++ src/examples/rxjs/error-handling/catch.js
@@ -2,13 +2,9 @@ export default
 `const { chart } = require('rp-api');
 const { Observable } = require('rxjs/Rx');
 
-const error$ = new Observable(observer=>{
-  const t1 = setTimeout(()=>{
-    observer.error('oh');
-  }, 5);
-  
-  return ()=>clearTimeout(t1);
-});
+const error$ = Observable
+  .timer(5)
+  .switchMap(() => Observable.throw('oh'));
   
 const catch$ = error$
   .catch(err => Observable.of(err));
