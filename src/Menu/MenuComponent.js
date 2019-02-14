a0994de488c54f26e1dcc6f765a663dcb0071b2d
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-02-14 02:58:08 +0200

    slightly modified menu to show only current items

--- src/Menu/MenuComponent.js
+++ src/Menu/MenuComponent.js
@@ -1,23 +1,60 @@
 import React, { Component } from 'react';
 import './MenuComponent.css';
 import { menu as examplesMenu } from '../examples';
-import { Link } from '@reach/router'
+import { Link, Match, Location } from '@reach/router'
 
 export class MenuComponent extends Component {
+    isLinkActive = ({ isPartiallyCurrent }) => {
+        return isPartiallyCurrent
+            ? { className: "Menu__Link active" }
+            : { className: "Menu__Link" }
+    }
+
+    renderLibraryMenu(root){
+        return (
+            <ul className="Menu__library-contents">
+                { root.library.groups.map(group => (
+                    <li key={group.name} className="Menu__group">
+                        <span className="Menu__group-name">{group.name}</span>
+                        <ul className="Menu__group-contents">
+                            {group.items.map(item => (
+                                <li key={item}
+                                    className="Menu__item"
+                                >
+                                    <Link
+                                        to={`/${root.handle}/${item}`}
+                                        getProps={this.isLinkActive}
+                                        key={item}
+                                    >{item}</Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+
     render() {
         return (
             <div className="Menu">
                 {
                     examplesMenu.map(root => (
                         <div key={root.handle} className="Menu__library-name">
-                            <Link to={ '/' + root.handle }>{root.library.name}</Link>
+                            <Link
+                                getProps={ this.isLinkActive }
+                                to={ '/' + root.handle }
+                                >{root.library.name}</Link>
                             <span className="Menu__library-version">v{root.library.version}</span>
                         </div>
                     ))
                 }
 
                 <div className="Menu__Gist">
-                    <Link to="/gist">Gist</Link>
+                    <Link
+                        getProps={ this.isLinkActive }
+                        to="/gist"
+                        >Load a gist</Link>
                 </div>
 
                 <hr />
@@ -25,24 +62,13 @@ export class MenuComponent extends Component {
                 <ul className="Menu__examples">
                     { examplesMenu.map(root => (
                         <li key={root.handle} className="Menu__library">
-                            <ul className="Menu__library-contents">
-                                { root.library.groups.map(group => (
-                                    <li key={ group.name } className="Menu__group">
-                                        <span className="Menu__group-name">{group.name}</span>
-                                        <ul className="Menu__group-contents">
-                                            { group.items.map(item => (
-                                                <li key={ item }>
-                                                    <Link
-                                                        to={`/${root.handle}/${item}`}
-                                                        className="Menu__item"
-                                                        key={item}
-                                                    >{item}</Link>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </li>
-                                ))}
-                            </ul>
+                            <Match path={ '/' + root.handle + '/*' }>{
+                                props =>
+                                    props.match
+                                    ? this.renderLibraryMenu(root) 
+                                    : null
+                            }
+                            </Match>
                         </li>
                     ))}
                 </ul>
