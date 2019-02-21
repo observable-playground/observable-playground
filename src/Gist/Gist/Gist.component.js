bbefbd476e5a2c82f093938c5b9cc92a84954255
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-02-21 14:12:52 +0200

    Added gist owner info

--- src/Gist/Gist/Gist.component.js
+++ src/Gist/Gist/Gist.component.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react'
 import { GistFileComponent } from './GistFile.component';
+import './Gist.css';
+import { GIST_URL_PREFIX } from './const';
 
 export class GistComponent extends Component {
     render(){
         const { data } = this.props;
-        const { files } = data;
-        const fileValues = Object.values(files)
+        const { owner } = data;
+        const files = Object.values(data.files);
+
+        const gistName = files[0].filename;
+
+        const fileValues = files
             .sort((a,b)=> a.filename.localeCompare(b.filename));
 
         return (
             <React.Fragment>
-                <div className="PageBlock">
-                    { data.description }
+                <div className="Gist__Header">
+                    <a
+                        className="Gist__Avatar"
+                        href={ 'https://gist.github.com/' + owner.login }
+                        target="_blank"
+                    >
+                        <img
+                            src={ owner.avatar_url }
+                            alt="Owner avatar"
+                        />
+                    </a>
+
+                    <div className="Gist__Header__Main">
+                        <div className="Gist__Path">
+                            <a
+                                className="Gist__UserName"
+                                href={ 'https://gist.github.com/' + owner.login }
+                                target="_blank"
+                            >{ owner.login }</a>
+                            &nbsp;/&nbsp;
+                            <a
+                                className="Gist__GistName"
+                                href={ GIST_URL_PREFIX + data.id }
+                                target="_blank"
+                            >{gistName}</a>
+                        </div>
+
+                        <div className="Gist__Description">
+                            { data.description }
+                        </div>
+                    </div>
                 </div>
+
                 <React.Fragment>
                     {
                         fileValues.map(file =>
