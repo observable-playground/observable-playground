172f3fd2a3a3c4d55cce097f63dc12fa13cd6983
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-02-26 01:53:58 +0200

    updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import {Link} from '@reach/router';
 
 export default () => (
-  <div>
-    <h1>404 - Oh no's! We couldn't find that page :(</h1>
+  <div className="PageBlock">
+    <h2>Page not found</h2>
+    <h3>or we might still be loading some assets...</h3>
+
+    <p>
+        If this playground wont load, you might:
+    </p>
+
+    <ul>
+        <li>Get to the <b><Link to="/">home page</Link></b> and start exploring new playgrounds</li>
+        <li>Check out <b><Link to="/rxjs/of/">RxJS of</Link></b> operator example and build on top of it</li>
+        <li>Submit an issue at <a href="https://github.com/observable-playground/observable-playground" target="_blank">github project page</a> for this case</li>
+    </ul>
+
+    <br />
   </div>
 )
