0d37821e4d8b503c56e81462fbcf2567b6128be8
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-03-22 14:46:39 +0200

    updated ssr code view to look similar to ace editor

--- src/Playground/SSRPlayground.js
+++ src/Playground/SSRPlayground.js
@@ -3,7 +3,16 @@ import './SSRPlayground.css';
 
 export const SSRPlayground = (props) => {
     return <div className="Playground">
-        <div className="Playground__editor"><pre className="SSRPlayground__Pre"><code>{ props.code }</code></pre></div>
-        <div className="Playground__chart"></div>
+        <div className="Playground__editor"><pre className="SSRPlayground__Pre"><code className="SSRPlayground__Code">{ props.code }</code></pre></div>
+        <div className="Playground__chart">
+            <div className="SSRPlayground__Loader">
+                <div className="lds-ellipsis">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
     </div>;
 }
