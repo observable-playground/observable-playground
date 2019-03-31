d86cd2a1f7b60afa20bd533ef6ee8e994a648ab9
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-04-01 02:02:34 +0300

    attempt to fix hydration issue with chart not having margin

--- src/Playground/SSRPlayground.js
+++ src/Playground/SSRPlayground.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { LoadingIndicator } from '../shared/LoadingIndicator/LoadingIndicator';
 import './SSRPlayground.css';
 
 export const SSRPlayground = (props) => {
     return <div className="Playground">
         <div className="Playground__editor"><pre className="SSRPlayground__Pre"><code className="SSRPlayground__Code">{ props.code }</code></pre></div>
         <div className="Playground__chart">
-            <div className="SSRPlayground__Loader">
+            <span className="SSRPlayground__Loader">
                 <LoadingIndicator />
-            </div>
+            </span>
         </div>
     </div>;
 }
