d5bfe3b85709cff84425957a53776ad101a8db1b
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-04-06 14:39:05 +0300

    updated forkJoin example

--- src/examples/rxjs/combination/forkJoin.js
+++ src/examples/rxjs/combination/forkJoin.js
@@ -1,15 +1,14 @@
 export default
 `const { rxObserver } = require('api/v0.3');
 const { timer, forkJoin } = require('rxjs');
-const { mapTo } = require('rxjs/operators');
+const { mapTo, take } = require('rxjs/operators');
 
 
 const a$ = timer(10).pipe(mapTo('a'));
-const b$ = timer(20).pipe(mapTo('b'));
+const b$ = timer(0, 10).pipe(take(3));
 
 const result$ = forkJoin(a$, b$);
 
 a$.subscribe(rxObserver('a$'));
 b$.subscribe(rxObserver('b$'));
-result$.subscribe(rxObserver('result$'));
-`
+result$.subscribe(rxObserver('forkJoin(a$, b$)'));`
