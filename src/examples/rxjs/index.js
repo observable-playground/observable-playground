94d28ae92764c9087ac649da449cba21b7fabf71
Kostia P <kosich@users.noreply.github.com>
2019-05-17 11:44:17 +0300

    added rxjs race to the menu

--- src/examples/rxjs/index.js
+++ src/examples/rxjs/index.js
@@ -24,6 +24,7 @@ export const library = {
             , items:
                 [ 'zip'
                 , 'forkJoin'
+                , 'race'
                 , 'combineLatest'
                 ]
             }
