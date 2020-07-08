9869afd464d6c950213f2261ce7bada6e08b367c
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2020-07-08 17:02:01 +0300

    fixed selected menu item indication

--- src/shared/Menu/MenuComponent.jsx
+++ src/shared/Menu/MenuComponent.jsx
@@ -6,35 +6,6 @@ import style from './MenuComponent.module.scss';
 
 const RXJS_MENU_ROOT = examplesMenu.find(({ handle }) => handle === 'rxjs');
 
-const isLinkActive = ({ isPartiallyCurrent }) => {
-    return isPartiallyCurrent
-        ? { className: style.Menu__Link_active }
-        : { className: style.Menu__Link }
-}
-
-const renderLibraryMenu = (menuEntry) => {
-    return (
-        <ul className={style.Menu__library_contents}>
-            {menuEntry.library.groups.map(group => (
-                <li key={group.name} className={style.Menu__group}>
-                    <span className={style.Menu__group_name}>{group.name}</span>
-                    <ul className={style.Menu__group_contents}>
-                        {group.items.map(item => (
-                            <li key={item}
-                                className={style.Menu__item}
-                            >
-                                <Link
-                                    href="/[libId]/[fileId]/"
-                                    as={`/${menuEntry.handle}/${item}/`}
-                                ><a className={style.Menu__Link}>{item}</a></Link>
-                            </li>
-                        ))}
-                    </ul>
-                </li>
-            ))}
-        </ul>
-    )
-}
 
 export function MenuComponent() {
     const router = useRouter();
@@ -47,29 +18,68 @@ export function MenuComponent() {
     return (
         <nav className={style.Menu}>
             {
-                examplesMenu.map(menuEntry => (
-                    <div key={menuEntry.handle} className={style.Menu__library_name}>
-                        <Link
-                            href="/[libId]/"
-                            as={ '/' + menuEntry.handle + '/' }
-                        >
-                            <a className={ style.Menu__Link }>{menuEntry.library.name}
-                            <span className={style.Menu__library_version}>v{menuEntry.library.version}</span>
-                        </a></Link>
+                examplesMenu.map(menuEntry => {
+                    const active = menuEntry == activeLib;
 
-                        { activeLib.handle == menuEntry.handle
-                        ? <ul className={style.Menu__examples}>
-                            <li key={activeLib.handle} className={style.Menu__library}>
-                                { renderLibraryMenu(activeLib) }
-                            </li>
-                          </ul>
-                        : null
-                        }
-                    </div>
-                ))
+                    return (
+                        <div key={menuEntry.handle} className={style.Menu__library_name}>
+                            <Link
+                                href="/[libId]/"
+                                as={ '/' + menuEntry.handle + '/' }
+                            >
+                                <a className={active ? style.Menu__Link_active : style.Menu__Link}>
+                                    {menuEntry.library.name} <span className={style.Menu__library_version}>v{menuEntry.library.version}</span>
+                                </a>
+                            </Link>
+
+                            { activeLib.handle == menuEntry.handle
+                            ? <ul className={style.Menu__examples}>
+                                <li key={activeLib.handle} className={style.Menu__library}>
+                                    <LibraryMenu menuEntry={activeLib} />
+                                </li>
+                            </ul>
+                            : null
+                            }
+                        </div>
+                    )
+                })
             }
-
-
         </nav>
     );
+}
+
+
+function LibraryMenu({ menuEntry }) {
+    const router = useRouter();
+
+    return (
+        <ul className={style.Menu__library_contents}>
+            {menuEntry.library.groups.map(group => (
+                <li key={group.name} className={style.Menu__group}>
+                    <span className={style.Menu__group_name}>{group.name}</span>
+                    <ul className={style.Menu__group_contents}>
+                        { group.items.map(item => {
+                            const href = `/${menuEntry.handle}/${item}/`;
+                            const active = router.asPath == `/${menuEntry.handle}/${item}/`;
+
+                            return (
+                                <li key={item}
+                                    className={style.Menu__item}
+                                >
+                                    <Link
+                                        href="/[libId]/[fileId]/"
+                                        as={href}
+                                    >
+                                        <a className={active ? style.Menu__Link_active : style.Menu__Link}>
+                                            {item}
+                                        </a>
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </li>
+            ))}
+        </ul>
+    )
 }
