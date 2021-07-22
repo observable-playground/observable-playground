8936a8db96030b6fb6957edef7430ee36264c32f
Kostia Palchyk <kosich@users.noreply.github.com>
2021-07-22 19:06:35 +0300

    [chore] added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/**
- * @type {import('next').NextConfig}
- */
-const config = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+})
+  
+const config = withBundleAnalyzer({
     trailingSlash: true
-};
+});
 
 module.exports = config;
